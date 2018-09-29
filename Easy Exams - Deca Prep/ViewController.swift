352409a3c6d705d450d00ddace2bd7f877fda8b3
sriharivishnu <srihari.vishnu@gmail.com>
2018-09-28 21:45:09 -0400

    Initial Commit

---
+++ Easy Exams - Deca Prep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Easy Exams - Deca Prep
+//
+//  Created by Srihari Vishnu on 2018-09-28.
+//  Copyright © 2018 Srihari Vishnu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
